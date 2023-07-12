6b6097b8e2aac513dd596f36db845b8c429a5640
Riku Miyata <amaguri0408@gmail.com>
2023-07-13 04:19:37 +0900

    add MySQLdb environment

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 app = Flask(__name__)
 app.config.from_object('app.config')
 db = SQLAlchemy(app)
