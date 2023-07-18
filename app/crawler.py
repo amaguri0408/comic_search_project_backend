8e9672e1bd110855e58179cd3d1512ae2cd8da2d
Riku Miyata <amaguri0408@gmail.com>
2023-07-19 03:31:23 +0900

    add サンデーうぇぶり crawler

--- app/crawler.py
+++ app/crawler.py
@@ -27,14 +27,24 @@ def exception(func):
             res['detail'] = str(e)
             status_code = 500
         else:
-            crawl_history = CrawlHistory(
-                app_id=self.app_record.id, 
-                comics_num=len(self.comics),
-                status='success'
-            )
-            res['status'] = 'success'
-            res['comics_num'] = len(self.comics)
-            status_code = 200
+            if self.comics:
+                crawl_history = CrawlHistory(
+                    app_id=self.app_record.id, 
+                    comics_num=len(self.comics),
+                    status='success'
+                )
+                res['status'] = 'success'
+                res['comics_num'] = len(self.comics)
+                status_code = 200
+            else:
+                crawl_history = CrawlHistory(
+                    app_id=self.app_record.id, 
+                    status='failure', 
+                    detail='no comics'
+                )
+                res['status'] = 'failure'
+                res['detail'] = 'no comics'
+                status_code = 500
         finally:
             db.session.add(crawl_history)
             db.session.commit()
@@ -47,6 +57,8 @@ class ComicCrawler:
         self.app_record = app_record
         if self.app_record.name == 'ガンガンONLINE':
             self.crawl_func = self._crawl_gangan_online
+        elif self.app_record.name == 'サンデーうぇぶり':
+            self.crawl_func = self._crawl_sunday_webry
         elif self.app_record.name == 'マガポケ':
             self.crawl_func = self._crawl_maga_poke
         elif self.app_record.name == 'マンガUP！':
@@ -101,6 +113,46 @@ class ComicCrawler:
                 'crawled_at': crawled_at,
             })
 
+
+    @exception
+    def _crawl_sunday_webry(self):
+        """id:12 サンデーうぇぶりの作品一覧を取得"""
+        load_url = urljoin(self.app_record.site_url, '/series')
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"
+        }
+        html = requests.get(load_url, headers=headers)
+        soup = BeautifulSoup(html.content, "html.parser")
+        datas_normal = soup.find_all("a", class_="webry-series-item-link")
+        # 夜サンデー
+        load_url = urljoin(self.app_record.site_url, '/series/yoru-sunday')
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"
+        }
+        html = requests.get(load_url, headers=headers)
+        soup = BeautifulSoup(html.content, "html.parser")
+        datas_yoru = soup.find_all("a", class_="webry-series-item-link")
+        
+        datas = datas_normal + datas_yoru
+        crawled_at = datetime.datetime.now()
+        for data in datas:
+            title = data.find("h4", class_="series-title").text
+            title_kana = self.conv.do(title)
+            author = data.find("p", class_="author").text
+            author_list = author.split('/')
+            main_author = ','.join(author_list[:min(2, len(author_list))])
+            sub_author = ','.join(author_list[min(2, len(author_list)):])
+            url = urljoin(self.app_record.site_url, data['href'])
+            self.comics.append({
+                'title': title,
+                'title_kana': title_kana,
+                'main_author': main_author,
+                'sub_author': sub_author,
+                'app_id': self.app_record.id,
+                'url': url,
+                'crawled_at': crawled_at,
+            })
+
     
     @exception
     def _crawl_maga_poke(self):
@@ -140,6 +192,7 @@ class ComicCrawler:
 
     @exception
     def _crawl_manga_up(self):
+        """id:26 マンガUP！の作品一覧を取得"""
         load_url = urljoin(self.app_record.site_url, 'original')
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"
@@ -163,7 +216,7 @@ class ComicCrawler:
                 new_author_list.extend(author_data.split('・'))
             main_author = ','.join(new_author_list[:min(2, len(new_author_list))])
             sub_author = ','.join(new_author_list[min(2, len(new_author_list)):])
-            url = urljoin(load_url, data.find("a")['href'])
+            url = urljoin(load_url + '/', data.find("a")['href'])
             self.comics.append({
                 'title': title,
                 'title_kana': title_kana,
