154f11a4d1779c91fa31bd427d5721613b5da3a4
Riku Miyata <amaguri0408@gmail.com>
2023-07-24 02:06:36 +0900

    add CORS setting

--- app/config.py
+++ app/config.py
@@ -4,4 +4,8 @@ DEBUG = True
 SQLALCHEMY_DATABASE_URI = getenv('RAILWAY_MYSQL_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
+CORS_ORIGINS = ["https://comic-serach.com", "http://localhost:8080"]
+
 APP_CSV_PATH = 'app_info.csv'
+
+DEPLOY_URL = 'https://comicsearchprojectbackend-production.up.railway.app/'
