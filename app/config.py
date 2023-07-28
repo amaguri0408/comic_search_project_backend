f3f9a239b7db0e8ff0529a1e8835c0467117deb8
Riku Miyata <amaguri0408@gmail.com>
2023-07-29 06:53:02 +0900

    CORS設定

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ DEBUG = True
 SQLALCHEMY_DATABASE_URI = getenv('RAILWAY_MYSQL_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
-CORS_ORIGINS = ["https://comic-serach.com", "http://localhost:8080", "http://192.168.2.100:8080"]
+CORS_ORIGINS = ["*"]
 
 APP_CSV_PATH = 'app_info.csv'
 
